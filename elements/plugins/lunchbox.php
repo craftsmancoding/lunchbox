a8f7506cbfa100c8697bf857dccbcebdccb15a62
Daniel Edano <daniel@craftsmancoding.com>
2014-10-15 13:07:40 +0800

    clean up

--- elements/plugins/lunchbox.php
+++ elements/plugins/lunchbox.php
@@ -17,16 +17,19 @@ switch ($modx->event->name) {
         break;  
 
     // Documents
-    case 'OnDocFormSave':        
+    case 'OnDocFormSave':
         if ('Lunchbox' == $resource->get('class_key')) {
                  // product_option_meta
-            $c = $modx->newQuery('modResource');
-            $c->where(array('parent'=>$resource->get('id')));        
-            $Children = $modx->getCollection('modResource', $c);
+        $c = $modx->newQuery('modResource');
+        $c->where(array('parent'=>$resource->get('id')));        
+        $Children = $modx->getCollection('modResource', $c);
             foreach ($Children as $child) {
                 $Child = $modx->getObject('modResource', array('id' => $child->get('id'),'show_in_tree' => 1));
-                $Child->set('show_in_tree', 0);
-                $Child->save();
+                if($Child) {
+                    $Child->set('show_in_tree', 0);
+                    $Child->save();
+                }
+              
             }
             
         }
